81012c160b1ffd19707dba3dcf7bf60c18f28011
Ümit Seren <uemit.seren@gmi.oeaw.ac.at>
2021-09-09 15:53:36 +0200

    Fix htcondor molecule test and copy task

--- roles/htcondor_ce/molecule/default/tests/test_default.py
+++ roles/htcondor_ce/molecule/default/tests/test_default.py
@@ -10,6 +10,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def test_package(host):
+    host.package('condor').is_installed
+    host.package('htcondor-ce').is_installed
+
+
+def test_service_running(host):
+    ce = host.service('condor-ce')
+    assert ce.is_enabled
+    assert ce.is_running
+
+
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
